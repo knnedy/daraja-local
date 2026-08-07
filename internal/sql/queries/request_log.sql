80a4f833209e431fa5b7bc8155f48bf2a6615b02
knnedy <kennymwendwadev@gmail.com>
2026-08-07 23:02:43 +0300

    feat(sql): add request_log query source for sqlc

---
+++ internal/sql/queries/request_log.sql
@@ -0,0 +1,13 @@
+-- name: CreateRequestLogEntry :one
+INSERT INTO "request_log" ("project_id", "kind", "direction", "status", "payload")
+VALUES (?, ?, ?, ?, ?)
+RETURNING *;
+
+-- name: ListRequestLogEntries :many
+SELECT * FROM "request_log"
+WHERE "project_id" = ?
+ORDER BY "created_at" DESC
+LIMIT ?;
+
+-- name: ClearRequestLog :exec
+DELETE FROM "request_log" WHERE "project_id" = ?;
