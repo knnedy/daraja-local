1e53de534cecd33ebbe496e0958169e31dc82619
knnedy <kennymwendwadev@gmail.com>
2026-08-13 13:33:22 +0300

    feat(cmd): add main.go, it wires config, repository, services, router, banner and a  graceful shutdown

---
+++ cmd/daraja-local/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io/fs"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/knnedy/daraja-local/internal/config"
+	"github.com/knnedy/daraja-local/internal/repository"
+	"github.com/knnedy/daraja-local/internal/router"
+	"github.com/knnedy/daraja-local/internal/service"
+)
+
+func main() {
+	if err := run(); err != nil {
+		slog.Error("daraja-local: fatal", "error", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	cfg, err := config.Load()
+	if err != nil {
+		return fmt.Errorf("load config: %w", err)
+	}
+
+	db, err := repository.Open(cfg.DBPath)
+	if err != nil {
+		return fmt.Errorf("open database: %w", err)
+	}
+	defer db.Close()
+
+	staticFS, err := fs.Sub(staticFiles, "out")
+	if err != nil {
+		return fmt.Errorf("prepare static assets: %w", err)
+	}
+
+	projectSvc := service.NewProjectService(db)
+	settingsSvc := service.NewSettingsService(db)
+	r := router.New(projectSvc, settingsSvc, staticFS)
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf("127.0.0.1:%d", cfg.Port),
+		Handler:      r,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	serveErr := make(chan error, 1)
+	go func() {
+		printBanner(srv.Addr, cfg.DBPath)
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			serveErr <- err
+			return
+		}
+		serveErr <- nil
+	}()
+
+	select {
+	case err := <-serveErr:
+		if err != nil {
+			return fmt.Errorf("serve: %w", err)
+		}
+	case <-ctx.Done():
+		slog.Info("daraja-local shutting down")
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := srv.Shutdown(shutdownCtx); err != nil {
+			return fmt.Errorf("shutdown: %w", err)
+		}
+	}
+
+	return nil
+}
